9df6c7967d758581d13a72627fabf4ec38cdca39
Rapafull <rapafull@gmail.com>
2019-01-11 22:28:23 -0600

    commit

--- server/routes/login.js
+++ server/routes/login.js
@@ -101,87 +101,87 @@ app.post('/google', async(req, res) => {
         });
     });
 
-    res.json({
-        token: token,
-        user: userGoogle
-    });
-    /*
-        if (userGoogle.email) {
-            //console.log(token);
-            //se verifica si el email esta registrado.
-            Usuario.findOne({ email: userGoogle.email }, (err, userDB) => {
-                // retorna un error al procesar la peticion.
-                if (err) {
-                    return res.status(500).json({
-                        status: false,
-                        err
-                    });
-                }
-                // si el email esta registrado. 
-                if (userDB) {
 
-                    // si el usuario antes habia iniciado sesion desde google.
-                    if (userDB.google) {
 
-                        // se verifica que el usuario este activado.
-                        if (!userDB.estado) {
-                            return res.status(400).json({
-                                status: false,
-                                message: 'El usuario esta desactivado.'
-                            });
+    if (userGoogle.email) {
+        //console.log(token);
+        //se verifica si el email esta registrado.
+        Usuario.findOne({ email: userGoogle.email }, (err, userDB) => {
+            // retorna un error al procesar la peticion.
+            if (err) {
+                return res.status(500).json({
+                    status: false,
+                    err
+                });
+            }
+            res.json({
+                token: token,
+                user: userGoogle,
+                userDB
+            });
+            // si el email esta registrado. 
+            if (userDB == null) {
+                // se instancia un nuevo esquema
+                let user = new Usuario({
+                    nombre: userGoogle.name,
+                    email: userGoogle.email,
+                    password: ':P',
+                    img: userGoogle.img,
+                    google: userGoogle.google
+                });
 
-                            // si esta activado se le concede el token.
-                        } else {
-                            let token = jwt.sign({ userDB }, process.env.KEY, { expiresIn: process.env.EXPIRE });
-                            res.json({
-                                status: true,
-                                userDB,
-                                token
-                            });
-                        }
+                //se guarda en la base de datos.
 
-                        //si esta registrado pero nunca ha iniado sesion con google.
-                    } else {
-                        return res.status(400).json({
+                user.save((err, userDB) => {
+                    if (err) {
+                        return res.status(500).json({
                             status: false,
-                            message: 'Debe iniciar sesion normalmente.'
+                            err
                         });
                     }
+                    // se le concede un token
+                    let token = jwt.sign({ userDB }, process.env.KEY, { expiresIn: process.env.EXPIRE });
 
-                    //Si el email no esta registrado.
-                } else {
-
-                    // se instancia un nuevo esquema
-                    let user = new Usuario({
-                        nombre: userGoogle.name,
-                        email: userGoogle.email,
-                        password: ':P',
-                        img: userGoogle.img,
-                        google: userGoogle.google
+                    //se imprime en pantalla.
+                    res.json({
+                        ok: true,
+                        userDB,
+                        token
                     });
+                });
+            } else {
+                // si el usuario antes habia iniciado sesion desde google.
+                if (userDB.google) {
 
-                    //se guarda en la base de datos.
+                    // se verifica que el usuario este activado.
+                    if (!userDB.estado) {
+                        return res.status(400).json({
+                            status: false,
+                            message: 'El usuario esta desactivado.'
+                        });
 
-                    user.save((err, userDB) => {
-                        if (err) {
-                            return res.status(500).json({
-                                status: false,
-                                err
-                            });
-                        }
-                        // se le concede un token
+                        // si esta activado se le concede el token.
+                    } else {
                         let token = jwt.sign({ userDB }, process.env.KEY, { expiresIn: process.env.EXPIRE });
-
-                        //se imprime en pantalla.
                         res.json({
-                            ok: true,
+                            status: true,
                             userDB,
                             token
                         });
+                    }
+
+                    //si esta registrado pero nunca ha iniado sesion con google.
+                } else {
+                    return res.status(400).json({
+                        status: false,
+                        message: 'Debe iniciar sesion normalmente.'
                     });
-                };
-            });
-        }*/
+                }
+
+
+            };
+        });
+    }
 });
 
 module.exports = app;
