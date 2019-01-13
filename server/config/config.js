9c6dd6a190b36547424101d6002ee83132e7daf3
Rapafull <rapafull@gmail.com>
2019-01-13 13:56:28 -0600

    Commit 11

--- server/config/config.js
+++ server/config/config.js
@@ -12,15 +12,19 @@ let entorno = process.env.NODE_ENV || 'dev';
 if (entorno === 'dev') {
     process.env.MONGO_URI = 'mongodb://localhost:27017/cafe';
 } else {
-    process.env.MONGO_URI = process.env.URI;
+    process.env.MONGO_URI = process.env.H_URI;
 }
 
 //key para el token
 //eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9
-process.env.KEY = process.env.KEY || 'misuperllavesecreta';
+process.env.KEY = process.env.H_KEY || 'misuperllavesecreta';
 
 //tiempo para el token
 process.env.EXPIRE = '1h';
 
 //google cliente ID
-process.env.CLIENT_ID = process.env.CLIENT_ID || '542751047808-p92m20c0gg1ak96irdiqq4g8re4hboh9.apps.googleusercontent.com';
+process.env.CLIENT_ID = process.env.H_CLIENT_ID || '542751047808-p92m20c0gg1ak96irdiqq4g8re4hboh9.apps.googleusercontent.com';
+
+// 542751047808-p92m20c0gg1ak96irdiqq4g8re4hboh9.apps.googleusercontent.com
+// process.env.KEY:       eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9
+// process.env.URI:       mongodb://cafe-user:Admin2113@ds153304.mlab.com:53304/cafe
