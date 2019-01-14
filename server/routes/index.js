49237f87c0c6bd90dbabba9fb75df59f0ab92574
Rapafull <rapafull@gmail.com>
2019-01-14 13:23:55 -0600

    Commit 17

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,5 +5,6 @@ app.use(require('./user'));
 app.use(require('./login'));
 app.use(require('./categoria'));
 app.use(require('./productos'));
+app.use(require('./upload'));
 
 module.exports = app;
---
+++ server/routes/upload.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const fileUpload = require('express-fileupload');
+const app = express();
+
+// default options
+app.use(fileUpload());
+
+app.post('/upload/:tipo/:id', function(req, res) {
+
+    if (!req.files) {
+        return res.status(400).json({
+            status: false,
+            err: { message: 'No se ha seleccionado ningun archivo' }
+        });
+    }
+    let tipos = req.params.tipo;
+    let id = req.params.id;
+
+    console.log(`Tipos: ${tipos}, id: ${id}`);
+
+
+    // let archivo = req.files.archivo;
+
+    // let archivoCortado = archivo.name.split('.');
+    // let extension = archivoCortado[archivoCortado.length - 1];
+    // let extensionesPermitidas = ['jpg', 'png', 'gif', 'jpeg'];
+    // if (extensionesPermitidas.indexOf(extension) < 0) {
+    //     return res.status(400).json({
+    //         status: false,
+    //         err: {
+    //             message: 'Extensiones permitidas.' + extensionesPermitidas.join(',')
+    //         }
+    //     });
+    // };
+
+    // res.send('OK');
+
+    // Use the mv() method to place the file somewhere on your server
+    // archivo.mv('uploads/filename.jpg', function(err) {
+    //     if (err)
+    //         return res.status(500).send(err);
+
+    //     res.send('File uploaded!');
+    // });
+
+});
+
+module.exports = app;
